767c0e424e8910b61bc41983394a7f918efc4661
Vicente Bermúdez <thevincentes@gmail.com>
2020-11-11 07:08:10 -0300

    Update gitignore to incldue IDEA Intellij

---
+++ src/main/java/mavi/ort/edu/uy/utils/StringUtils.java
@@ -0,0 +1,15 @@
+package mavi.ort.edu.uy.utils;
+
+import java.util.regex.Pattern;
+
+public class Utils {
+    private static final Pattern DOUBLE_PATTERN = Pattern.compile(
+            "[\\x00-\\x20]*[+-]?(NaN|Infinity|((((\\p{Digit}+)(\\.)?((\\p{Digit}+)?)" +
+                    "([eE][+-]?(\\p{Digit}+))?)|(\\.((\\p{Digit}+))([eE][+-]?(\\p{Digit}+))?)|" +
+                    "(((0[xX](\\p{XDigit}+)(\\.)?)|(0[xX](\\p{XDigit}+)?(\\.)(\\p{XDigit}+)))" +
+                    "[pP][+-]?(\\p{Digit}+)))[fFdD]?))[\\x00-\\x20]*");
+
+    public static boolean isDouble(String str) {
+        return DOUBLE_PATTERN.matcher(str).matches();
+    }
+}
